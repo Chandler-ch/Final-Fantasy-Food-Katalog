b856055963e109767605d4c9c63a008ba9c6284f
Chandler-ch <rchandler@mail.ch>
2026-09-19 23:29:09 +0200

    FFF-2: made a basic card without styling

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Gridcard from './components/Gridcard.vue'
+const index = 1
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <h1>Final Fantasy 15 Food Catalog</h1>
+  <Gridcard :index="index" title="Auberginenauflauf"></Gridcard>
 </template>
 
 <style scoped></style>
---
+++ src/components/Gridcard.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+interface Props {
+  index: Number
+  title: string
+}
+
+const props = defineProps<Props>()
+</script>
+<template>
+  <div class="container">
+    <img />
+    <span class="subtitle">{{ index }}</span>
+    <h2 class="title">{{ title }}</h2>
+  </div>
+</template>
+<style scoped></style>
